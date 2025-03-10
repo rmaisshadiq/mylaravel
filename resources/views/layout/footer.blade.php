86f78e11017bf85cbd5c99b0d4737007120e5a83
NabilAchmad <nabilachmad316@gmail.com>
2025-03-10 08:50:15 +0700

    commit

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-dark mt-auto py-3 text-white">
+    <div class="container">
+        Copyrigth &copy; {{date("Y")}} PNP.
+    </div>
+</footer>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,29 @@
+<header>
+    {{-- <! -- Fixed navbar --> --}}
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Akademik</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation" <span
+                    class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul
+                        class="navbar-nav me-auto mb-2 mb-md-0                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/home">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/mahasiswa">Mahasiswa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/dosen">Dosen</a>
+                            </l>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/prodi">Prodi</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+</header>
