8791bf7fbc7d133af15d85cf117051e1927024f6
NabilAchmad <nabilachmad316@gmail.com>
2025-03-10 09:13:25 +0700

    commit

---
+++ resources/views/akademik/prodipnp.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.main')
+@section('title', 'Daftar Prodi')
+@endsection
+@section('content')
+    <h1>Daftar Prodi Jurusan Teknologi Informasi</h1>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="/images/logoti.png" class="card-img-top" alt="Logo TI">
+                    <div class="card-body">
+                        <h5 class="card-title">Prodi Manajemen Informatika</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nihil voluptate vero
+                            harum soluta reiciendis </p>
+                        <a href="#" class="btn btn-primary">more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="/images/logoti.png" class="card-img-top" alt="Logo TI">
+                    <div class="card-body">
+                        <h5 class="card-title">Prodi Teknik Komputer</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nihil voluptate vero
+                            harum soluta reiciendis </p>
+                        <a href="#" class="btn btn-primary">more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="/images/logoti.png" class="card-img-top" alt="Logo TI">
+                    <div class="card-body">
+                        <h5 class="card-title">Prodi Teknologi Rekayasa Perangkat Lunak</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nihil voluptate vero
+                            harum soluta reiciendis </p>
+                        <a href="#" class="btn btn-primary">more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
