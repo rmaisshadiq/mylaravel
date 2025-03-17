5e0a119bff4b82e7fac171ff8a8ac311c73e4a21
NabilAchmad <nabilachmad316@gmail.com>
2025-03-17 09:11:06 +0700

    commit

--- database/migrations/2025_03_17_015655_create_dosens_table.php
+++ database/migrations/2025_03_17_015655_create_dosens_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('mahasiswa', function (Blueprint $table) {
+        Schema::create('dosens', function (Blueprint $table) {
             $table->id();
-            $table->string('nama');
-            $table->string('nim')->unique();
-            $table->string('jurusan');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('mahasiswa');
+        Schema::dropIfExists('dosens');
     }
 };
