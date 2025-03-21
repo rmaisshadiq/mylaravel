ac6291bd0696e73bc2b1d59d65fa34ff0e2e6faf
NabilAchmad <nabilachmad316@gmail.com>
2025-03-21 15:27:13 +0700

    commit

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -18,6 +18,75 @@ class MahasiswaController extends Controller
         return "Data berhasil ditambahkan";
     }
 
+    public function insert_prepared(){
+        $query = DB::insert('insert into mahasiswas (nim, nama, jurusan, email, nohp, prodi, alamat, tgllahir, created_at, updated_at) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', ["2311082032", "Achmad", "Teknologi Informasi", "nabilachmad715@gmail.com", "081367995046", "RPL", "Padang", "2005-02-20", now(), now()]);
+        return "Data berhasil ditambahkan";
+    }
+
+    public function insert_binding(){
+        $query = DB::insert('insert into mahasiswas (nim, nama, jurusan, email, nohp, prodi, alamat, tgllahir, created_at, updated_at) values (:nim, :nama, :jurusan, :email, :nohp, :prodi, :alamat, :tgllahir, :created_at, :updated_at)',
+        [
+            "nim"=>"2311082031",
+            "nama"=>"Achmad",
+            "jurusan"=>"Teknologi Informasi",
+            "email"=>"nabilachmad7@gmail.com",
+            "nohp"=>"081367995046",
+            "prodi"=>"RPL",
+            "alamat"=>"Padang",
+            "tgllahir"=>"2005-02-20",
+            "created_at"=>now(),
+            "updated_at"=>now()
+            ]
+        );
+        return "Data berhasil ditambahkan";
+    }
+
+    public function update_sql(){
+        $query = DB::update("UPDATE mahasiswas SET jurusan = 'sastra mesin' where nama = ?", ["Achmad"]);
+        return "Data berhasil diubah";
+    }
+
+    public function delete_sql(){
+        $query = DB::update("DELETE FROM mahasiswas where nama = ?", ["Nabil"]);
+        return "Data berhasil dihapus";
+    }
+
+    public function select_sql(){
+        $query = DB::select("SELECT * FROM mahasiswas");
+        dd($query);
+    }
+
+    public function selectTampil(){
+        $query = DB::select("SELECT * FROM mahasiswas");
+        echo ($query[1]->id) . "<br>";
+        echo ($query[1]->nim) . "<br>";
+        echo ($query[1]->nama) . "<br>";
+        echo ($query[1]->jurusan) . "<br>";
+        echo ($query[1]->email) . "<br>";
+        echo ($query[1]->nohp) . "<br>";
+        echo ($query[1]->prodi) . "<br>";
+        echo ($query[1]->alamat) . "<br>";
+        echo ($query[1]->tgllahir) . "<br>";
+        echo ($query[1]->created_at) . "<br>";
+        echo ($query[1]->updated_at) . "<br>";
+
+    }
+
+    public function selectView(){
+        $query = DB::select("SELECT * FROM mahasiswas");
+        return view('akademik.mahasiswapnp', ['mhs' => $query]);
+    }
+
+    public function selectWhere(){
+        $query = DB::select("SELECT * FROM mahasiswas where prodi = ? ORDER BY nim ASC", ['RPL']);
+        return view('akademik.mahasiswapnp', ['mhs' => $query]);
+    }
+
+    public function statement(){
+        $query = DB::delete("TRUNCATE mahasiswas");
+        return "Data berhasil dihapus";
+    }
+
     public function index()
     {
         //
--- resources/views/akademik/mahasiswapnp.blade.php
+++ resources/views/akademik/mahasiswapnp.blade.php
@@ -1,12 +1,29 @@
+Update File mahasiswapnp.blade.php
+
 @extends('layouts.main')
 @section('title')
     Daftar Mahasiswa
 @endsection
 @section('content')
-    <h1>Daftar Mahasiswa Jurusan Teknologi Informasi</h1>
-    <ol>
-        @foreach ($mhs as $namaMhs)
-            <li>{{ $namaMhs }}</li>
-        @endforeach
-    </ol>
+    <h1>Daftar Mahasiswa jurusan ti</h1>
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+                <th>Nama</th>
+                <th>NIM</th>
+                <th>Jurusan</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($mhs as $mahasiswa)
+                <tr>
+                    <td>{{ $mahasiswa->id}}</td>
+                    <td>{{ $mahasiswa->nama  }} </td>
+                    <td>{{ $mahasiswa->nim  }} </td>
+                    <td>{{ $mahasiswa->prodi  }} </td>
+
+                </tr>
+            @endforeach
+    </table>
 @endsection
